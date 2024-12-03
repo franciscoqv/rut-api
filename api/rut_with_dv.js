a16d56e932681903dadf02ed9bd83d97a352923d
Francisco Quiroga Vergara <pancho.qv@gmail.com>
2024-12-03 17:23:35 -0300

    Update rut_with_dv.js

--- api/rut_with_dv.js
+++ api/rut_with_dv.js
@@ -8,7 +8,7 @@ export default function handler(req, res) {
       }
 
       // Delete points and dashes from the RUT
-      const cleanedRUT = RUT.replace(/[.-]/g, '');
+      var cleanedRUT = RUT.replace(/[.-]/g, '');
 
       // Delete prefixed zeros (e.g., 0028393044 --> 28393044)
       cleanedRUT = Number(cleanedRUT.slice(0, -1)).toString() + cleanedRUT.slice(-1);
